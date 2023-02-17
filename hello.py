96f040ff6ddc626655ab569c54d81244113dd61d
Bridget Smith Epaul <bridget.smith_epaul@uconn.edu>
2023-02-17 11:46:01 -0500

    Removing temperature import

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from temperature import TemperaturePlot
 
 app = Flask(__name__)
 
@@ -7,7 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('home.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
