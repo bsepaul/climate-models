74d675142b153b7e45ea995bb09b1c90a7b388f5
Bridget Smith Epaul <bridget.smith_epaul@uconn.edu>
2023-03-28 11:36:05 -0400

    Updating requirements and temperature.py to produce correct graph

--- temperature.py
+++ temperature.py
@@ -1,46 +1,37 @@
-# IMPORTS FROM EXAMPLES
-# from cartopy.mpl.geoaxes import GeoAxes
-# from matplotlib import cm
-# from mpl_toolkits.axes_grid1 import AxesGrid
-# import geocat.datafiles as gdf
 import numpy as np
 import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-# import numpy as np
-from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
 import geocat.viz as gv
-import matplotlib.animation as animation
-import math
 import xarray as xr
-import mpld3
 import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize
+
 
 class TemperaturePlot:
 
     def __init__(self, months, color="viridis", central_longitude=0):
-        self.ds     = None   # ds is the data from the files
-        self.data   = None   # data will store the data that the graph is going to be made from
-        self.months = months # months is an array of the months that the user wants to be averaged for their plot
-        self.label  = ""     # label of the units on color bar
-        self.title  = ""     # title of the plot
-        self.color  = color  # color scheme of the plot
+        self.ds = None   # ds is the data from the files
+        self.data = None   # data will store the data that the graph is going to be made from
+        # months is an array of the months that the user wants to be averaged for their plot
+        self.months = months
+        self.label = ""     # label of the units on color bar
+        self.title = ""     # title of the plot
+        self.color = color  # color scheme of the plot
         self.central_longitude = central_longitude
-
 
     def set_data(self):
 
         # get the data from the first month in the list of months
         first_month = self.months[0]
-        file = "netcdf_files/b.e12.B1850.T31_g37.1x.cam.h0.3000-{}.nc".format(first_month)
+        file = "netcdf_files/b.e12.B1850.T31_g37.1x.cam.h0.3000-{}.nc".format(
+            first_month)
         self.ds = xr.open_dataset(file, decode_times=False)
         print("collecting data from file: {}".format(file))
         self.label = "K"
-        self.title = "Global Surface Temperature" 
+        self.title = "Global Surface Temperature"
         try:
             # Go through each month's file and sum  precipitation values, TMQ
             for month in self.months[1:]:
-                file = "netcdf_files/b.e12.B1850.T31_g37.1x.cam.h0.3000-{}.nc".format(month)
+                file = "netcdf_files/b.e12.B1850.T31_g37.1x.cam.h0.3000-{}.nc".format(
+                    month)
                 print("collecting data from file: {}".format(file))
                 next_ds = xr.open_dataset(file, decode_times=False)
                 self.ds.TS.values += next_ds.TS.values
@@ -51,7 +42,7 @@ class TemperaturePlot:
         except AttributeError:
             print("Dataset is missing \'TS\' attribute")
             exit()
-        
+
         # Another error occurred while accessing the data
         except:
             print("Something went wrong while accessing the data file")
@@ -60,10 +51,10 @@ class TemperaturePlot:
         # Average the values by dividing the values by the number of months
         self.data.values = (self.data.values) / len(self.months)
 
-
     def make_graph(self):
-        fig = plt.figure(figsize=(10,6))
-        ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=self.central_longitude))
+        fig = plt.figure(figsize=(10, 6))
+        ax = plt.axes(projection=ccrs.PlateCarree(
+            central_longitude=self.central_longitude))
 
         # add coastlines
         ax.coastlines(linewidths=0.5)
@@ -72,10 +63,10 @@ class TemperaturePlot:
 
         # Use geocat.viz.util convenience function to set axes limits & tick values
         gv.set_axes_limits_and_ticks(ax,
-                                    xlim=(-180, 180),
-                                    ylim=(-90, 90),
-                                    xticks=np.linspace(-180, 180, 13),
-                                    yticks=np.linspace(-90, 90, 7))
+                                     xlim=(-180, 180),
+                                     ylim=(-90, 90),
+                                     xticks=np.linspace(-180, 180, 13),
+                                     yticks=np.linspace(-90, 90, 7))
 
         # Use geocat.viz.util convenience function to add minor and major tick lines
         gv.add_major_minor_ticks(ax, labelsize=10)
@@ -83,34 +74,18 @@ class TemperaturePlot:
         # Use geocat.viz.util convenience function to make latitude, longitude tick labels
         gv.add_lat_lon_ticklabels(ax)
 
-
         plot = self.data[0, :, :].plot.contourf(ax=ax,
                                                 transform=ccrs.PlateCarree(),
                                                 levels=120,
                                                 cmap=self.color,
                                                 add_colorbar=False)
 
-        # cbar = plt.colorbar(plot,
-        #                     orientation='horizontal',
-        #                     shrink=0.9,
-        #                     extendrect=True)
+        cbar = plt.colorbar(plot,
+                            orientation='horizontal',
+                            shrink=0.9,
+                            extendrect=True)
 
-        # cbar.ax.tick_params(labelsize=10)
-
-        # fig.colorbar(plt.cm.ScalarMappable(norm=Normalize(0, 1), cmap=self.color))
-        # fig.colorbar(plt.cm.ScalarMappable(norm = Normalize(192,312),
-                                    # vmin=192,
-                                    # vmax = 312,
-                                    # levels=120, # number of different temperature levels
-                                    # cmap = self.color))
-                                    # cbar_kwargs={
-                                    #     "extendrect":True,
-                                    #     "orientation":"horizontal",
-                                    #     "format":"%.0f",
-                                    #     # "ticks": np.arange(192, 313, 10),
-                                    #     "label":self.label,
-                                    #     "shrink": 0.90}))
-
+        cbar.ax.tick_params(labelsize=10)
 
         gv.set_titles_and_labels(
             ax=ax,
@@ -118,21 +93,12 @@ class TemperaturePlot:
             xlabel="Longitude",
             ylabel="Latitude")
 
-        fig.savefig('static/plot.png')
-
-
-        # html_str = mpld3.fig_to_html(fig)
-        # html_file= open("index.html","w")
-        # html_file.write(html_str)
-        # html_file.close()
-    
+        fig.savefig('static/plot2.png')
 
     def create_plot(self):
         self.set_data()
         self.make_graph()
 
 
-
-
-# testPlot = TemperaturePlot(["01", "02", "03", "04"])
-# testPlot.create_plot()
+testPlot = TemperaturePlot(["01", "02", "03", "04"])
+testPlot.create_plot()
