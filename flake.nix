832fefc8508a1832e31550b943147e08b723e5b8
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-03-29 12:35:59 -0400

    fixing cmaps

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+# THis is my current flake.nix but I am getting the following error
 {
   inputs = {
     nixpkgs = {
@@ -5,6 +6,9 @@
     };
     flake-utils = {
       url = "github:numtide/flake-utils";
+    };
+    cmaps-src = {
+      url = "github:hhuangwx/cmaps";
     };
   };
   outputs = {
@@ -17,6 +21,87 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        inherit (pkgs) stdenv lib;
+
+        pythonPackages = lib.fix' (self:
+          with self;
+            pkgs.python3Packages
+            // {
+              cartopy = buildPythonPackage rec {
+                pname = "cartopy";
+                version = "0.21.0";
+
+                src = fetchPypi {
+                  inherit pname version;
+                  sha256 = "sha256-zh06KKEy6UyJrDN2mlD4H2VjSrK9QFVjF+Fb1srRzkI=";
+                };
+
+                postPatch = ''
+                  # https://github.com/SciTools/cartopy/issues/1880
+                  substituteInPlace lib/cartopy/tests/test_crs.py \
+                    --replace "test_osgb(" "dont_test_osgb(" \
+                    --replace "test_epsg(" "dont_test_epsg("
+                '';
+
+                nativeBuildInputs = [
+                  cython
+                  geos # for geos-config
+                  proj
+                  setuptools-scm
+                ];
+
+                buildInputs = [
+                  geos
+                  proj
+                ];
+
+                propagatedBuildInputs = [
+                  # required
+                  six
+                  pyshp
+                  shapely
+                  numpy
+
+                  # optional
+                  gdal
+                  pillow
+                  matplotlib
+                  pyepsg
+                  pykdtree
+                  scipy
+                  fiona
+                  owslib
+                ];
+
+                checkInputs = [pytestCheckHook filelock mock pep8 flufl_lock];
+                pytestFlagsArray = [
+                  "--pyargs"
+                  "cartopy"
+                  "-m"
+                  "'not network and not natural_earth'"
+                ];
+
+                disabledTests = [
+                  "test_nightshade_image"
+                  "background_img"
+                  "test_gridliner_labels_bbox_style"
+                ];
+              };
+
+              cmaps = buildPythonPackage rec {
+                pname = "cmaps";
+                version = "0.1.0";
+                src = cmaps-src;
+                meta = with pkgs.stdenv.lib; {
+                  description = "Colormaps for scientific visualization.";
+                  homepage = "https://github.com/hhuangwx/cmaps";
+                  license = licenses.mit;
+                  maintainers = [maintainers.yourname];
+                };
+              };
+              # geocat.viz =  buildPythonPackage rec {
+              # metpy =  buildPythonPackage rec {
+            });
       in rec {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -25,8 +110,9 @@
                 certifi
                 charset-normalizer
                 click
+                wheel
                 # cartopy
-                # cmaps
+                cmaps
                 # geocat.viz
                 # metpy
                 contourpy
@@ -71,4 +157,3 @@
       }
     );
 }
-
