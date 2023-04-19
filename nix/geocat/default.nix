c8ec7749e25ce2a8f29bed2c9011876c6db24c04
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-04-18 20:00:24 -0400

    few changes

--- nix/geocat/default.nix
+++ nix/geocat/default.nix
@@ -1,22 +1,42 @@
-{pkgs ? import <nixpkgs> {}}: let
-  geocatViz = pkgs.python3Packages.buildPythonPackage {
-    pname = "geocat-viz";
-    version = "0.6.0";
-    src = pkgs.fetchgit {
-      url = "https://github.com/NCAR/geocat-viz.git";
-      sha256 = "sha256-p7GYHkkiVbTVhJY/+uNnfQbGe71CvYk9DBaogUwgPnU=";
+{}:
+let
+  nixpkgs = (fetchTarball "https://github.com/NixOS/nixpkgs/archive/0874168639713f547c05947c76124f78441ea46c.tar.gz");
+  pkgs = import nixpkgs { };
+  buildPythonPackage = pkgs.python3Packages.buildPythonPackage;
+  fetchPypi = pkgs.python3Packages.fetchPypi;
+
+  metpy = buildPythonPackage rec{
+    pname = "MetPy";
+    version = "1.4.1";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-oT3S2jYOv9hWJw5BdG5P1Uutyp3NvYASKegDgs4x27k=";
     };
-    propagatedBuildInputs = [
-      pkgs.python3
-      pkgs.python39Packages.wheel
-    ];
-    buildInputs = [
-      # pkgs.cartopy
-      pkgs.python39Packages.setuptools
-      pkgs.python39Packages.xarray
-      pkgs.python39Packages.matplotlib
-      pkgs.python310Packages.cartopy
-    ];
+    propagatedBuildInputs = with pkgs.python3Packages ;[ setuptools_scm pyproj numpy xarray pint scipy traitlets pooch matplotlib ];
+  };
+  cmaps = buildPythonPackage rec{
+    pname = "cmaps";
+    version = "1.0.5";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-jucIv2xAJNzQYNZYqgy7p0CsBHLlY4UStHZN6h6SjEY=";
+    };
+    propagatedBuildInputs = with pkgs.python3Packages ;[ numpy matplotlib ];
+  };
+
+  goecat-viz = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "geocat.viz";
+    version = "2022.7.0";
+    src = pkgs.python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-7iBZitt+A/BJVsBahlVAtUfps1b0jKBBsqpPWBoqhPI=";
+    };
+    propagatedBuildInputs = with pkgs.python3Packages; [ cmaps metpy cartopy xarray scikit-learn pint traitlets pooch ];
   };
 in
-  geocatViz
+pkgs.mkShell {
+  pname = "geocat-shell";
+  buildInputs = [
+    (pkgs.python3.withPackages (ps: [ goecat-viz ]))
+  ];
+}
